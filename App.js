3b3cd3b47bd00b711428f2e89326c8aabfa26f14
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-04-28 17:45:25 +0200

    untill pass project to expo bare

--- App.js
+++ App.js
@@ -23,11 +23,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import "expo-dev-client"//! to authentification eas build
 
 
-//ios   :    1014731523069-46q5a8689krdt10mmbnafa5lcnnn1af9.apps.googleusercontent.com
-//android : 1014731523069-9ilas3r1shkv09ieu3osforca7sevrgv.apps.googleusercontent.com
-
-
-
 export default function App() {
 
 
--- screens/mainComponent/ChatMain.js
+++ screens/mainComponent/ChatMain.js
@@ -1,22 +1,133 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import React, { useEffect, useState } from "react";
+
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+
+//! 3 keys to change on .ENV
+
+WebBrowser.maybeCompleteAuthSession();
 
 const ChatMain = () => {
+  const [accessToken, setAccesToken] = React.useState(null);
+  const [user, setUser] = React.useState(null);
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+    clientId:
+      "621288007374-msitiqg7s0tg0tgbvkmlif4tdf4nu0bp.apps.googleusercontent.com",
+    iosClientId:
+      "621288007374-qabpj251e7v19q99ae69hjd5t7qelbto.apps.googleusercontent.com",
+    androidClientId:
+      "621288007374-me0i12d32gahc298191r7vv0rda2gqiv.apps.googleusercontent.com",
+  });
+
+  useEffect(() => {
+    if (response?.type === "success" && response?.authentication) {
+      console.log(response.authentication.accessToken);
+      setAccesToken(response.authentication.accessToken);
+      accessToken && fetchUserInfo();
+    }
+  }, [response, accessToken]);
+
+  const ShowUserInfo = () => {
+    if (user) {
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Welcome</Text>
+          <Image
+            source={{ uri: user.picture }}
+            style={{ width: 100, height: 100, borderRadius: 50 }}
+          ></Image>
+          <Text>{user.name}</Text>
+          <View>
+          </View>
+
+        </View>
+      );
+    }
+  };
+
+
+
+  const fetchUserInfo = async () => {
+    const response = await fetch(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    const userInfo = await response.json();
+    setUser(userInfo);
+  };
+
   return (
-
-
     <SafeAreaView style={styles.container}>
-      <Text>ChatMain</Text>
-    </SafeAreaView>
-  )
-}
+      {user && <ShowUserInfo />}
 
-export default ChatMain
+      {user === null && (
+        <>
+          <View style={{ justifyContent: "center", alignItems: "center" , width:"100%", gap:10}}>
+            <Text>Signup</Text>
+
+            <View
+            style={{
+              width: "90%"
+            }}
+            >
+
+            <TouchableOpacity
+              disabled={!request}
+              onPress={() => {
+                promptAsync();
+              }}
+            >
+              <View
+                style={{
+                  paddingTop: 15,
+                  paddingBottom: 15,
+                  backgroundColor: "#FFB25F",
+                  borderRadius: 10,
+                  width:"100%",
+                  justifyContent:"center",
+                  alignItems:"center"
+                }}
+              >
+                <Text
+                  style={{
+                    color: "#fff",
+                  }}
+                >
+                  Button
+                </Text>
+              </View>
+            </TouchableOpacity>
+            </View>
+
+          </View>
+        </>
+      )}
+    </SafeAreaView>
+  );
+};
+
+export default ChatMain;
 
 const styles = StyleSheet.create({
-    container:{
-        justifyContent:"center",
-        alignItems:"center",
-        flex: 1
-    }
-})
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+});
